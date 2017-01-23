6726c6b4b33d90fa1590363a458b4f13817a4c30
Artem <llaminator@users.noreply.github.com>
2017-01-23 10:11:30 +0300

    Update NB.h

--- NB/NB.h
+++ NB/NB.h
@@ -1,12 +1,20 @@
-/*Kuzminov Artem 10-4, 2017/16/01*/
+/* Kuzminov Artem, 10-4, 23/01/2017 */
 
 #define Max 50
 
-typedef struct Pupil 
-{
-	char Number [30];
-	int Class;
-	int Sex;
-}  Pupil;
+typedef struct {
+  int Day, Month, Year;
+} Date;
 
-int AddPupil(int cur, Pupil DB);
+typedef struct {
+  Date Birthday;
+  char Surname[30];
+  char Name[30];
+  char Number[22];
+  int Age;
+  int Form;
+  char Sex;
+  char FromDigit;
+} Pupil;
+
+int AddPupil(int cur, Pupil *DB);
