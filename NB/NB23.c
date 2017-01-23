498827ac9aa2bfff2db3277b4e65335b2265815a
Artem <llaminator@users.noreply.github.com>
2017-01-23 10:11:15 +0300

    Update NB23.c

--- NB/NB23.c
+++ NB/NB23.c
@@ -1,68 +1,41 @@
 /*Kuzminov Artem 10-4, 2017/16/01*/
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "nb.h"
-	
-void Menu(int cur, Pupil DB)
+
+#define Max 50
+
+int Menu( int cur, Pupil *DB )
 {
-	pritnf("0 - Exit\n"
-		"1 - Add pupil\n"
-		"2 - Delete by number\n"
-		"3 - Output all pupils\n"
-		"4 - Sort datebase\n"
-		"5 - Flip database\n"
-		"6 - Save database\n"
-		"7 - load database\n");
-	switch (getch())
-	{
-		case '1':
-		{
-			cur += AddPupil(cur, DB);
-			break;
-		}
-		/*case '2':
-		{
-			DelPupil(cur, DB);
-			break;
-		}
-		case '3':
-		{
-			Output(cur, DB);
-			break;
-		}
-		case '4':
-		{
-			char ch;
-			printf("a - by number\nb - by smth else\n");
-			scanf("%c", &ch);
-			SortByValue(cur, DB);
-			break;
-		}
-		case '5':
-		{
-			FlipDb(cur, DB);
-			break;
-		}
-		case '6':
-		{
-			SaveDB(cur, DB);
-			break;
-		}
-		case '7':
-		{
-			LoadDB(cur, DB);
-			break;
-		}*/
-	}
+  printf("0 - Exit\n1 - Add pupil\n2 - Delete by number\n3 - Output all pupils\n4 - Sort datebase\n5 - Flip database\n6 - Save database\n7 - load database\n");
+  switch(getch())
+  {
+  case '0':
+    return 0;
+  case '1':
+    AddPupil(cur, DB);
+    break;
+  case '2':
+    break;
+  case '3':                      
+    break;
+  case '4':
+    break;
+  case '5':
+    break;
+  case '6':
+    break;
+  case '7':
+    break;
+  }
 }
 
-int main(void)
+int main( void )
 {
-	int cur = 0;
-	Pupil DB[Max];
-	Menu(cur, DB);
-	return 0;
+  int cur = 0;
+  Pupil DB[50];
+  while (Menu(cur, DB))
+    cur += Menu(cur, DB);
+  return 0;
 }
