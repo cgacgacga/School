e803345c94ca0feba5b7a3218c22ea750a25e880
Artem <llaminator@users.noreply.github.com>
2017-01-24 00:35:05 +0300

    First 3 stages are currently working

--- NB/NB23.c
+++ NB/NB23.c
@@ -1,41 +1,77 @@
 /*Kuzminov Artem 10-4, 2017/16/01*/
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "nb.h"
 
 #define Max 50
 
-int Menu( int cur, Pupil *DB )
+void GetStr1(char *name, int n)
 {
-  printf("0 - Exit\n1 - Add pupil\n2 - Delete by number\n3 - Output all pupils\n4 - Sort datebase\n5 - Flip database\n6 - Save database\n7 - load database\n");
-  switch(getch())
-  {
-  case '0':
-    return 0;
-  case '1':
-    AddPupil(cur, DB);
-    break;
-  case '2':
-    break;
-  case '3':                      
-    break;
-  case '4':
-    break;
-  case '5':
-    break;
-  case '6':
-    break;
-  case '7':
-    break;
-  }
+	int i;
+	for (i = 0; i < n; i++)
+		name[i] = 0;
+	for (i = 0; i< n; i++)
+	{
+		scanf("%c", &name[i]);
+		if (name[i] == '\n')
+		{
+			name[i] = 0;
+			break;
+		}
+	}
+
+
 }
 
-int main( void )
+int Menu(int cur, Pupil *DB)
 {
-  int cur = 0;
-  Pupil DB[50];
-  while (Menu(cur, DB))
-    cur += Menu(cur, DB);
-  return 0;
+	char num[22];
+	int ch = 0;
+	printf("0 - Exit\n1 - Add pupil\n2 - Delete by number\n3 - Output all pupils\n4 - Sort datebase\n5 - Flip database\n6 - Save database\n7 - load database\n");
+	switch (getch())
+	{
+	case '0':
+		return 2;
+	case '1':
+		ch = AddPupil(cur, DB);
+		if (ch == 1)
+			printf("Failed to add a pupil");
+		return 1;
+	case '2':
+		GetStr1(num, 22);
+		ch = DeleteByNumber(cur, DB, num);
+		if (ch == 1)
+			printf("Failed to delete a pupil");
+		return -1;
+	case '3':
+		ch = Output(cur, DB);
+		if (ch == 1)
+			printf("Failed to output");
+		return 0;
+	/*case '4':
+		break;
+	case '5':
+		break;
+	case '6':
+		break;
+	case '7':
+		break;
+	*/
+	}
+}
+
+int main(void)
+{
+	int cur = 0,c = 1;
+	Pupil DB[50];
+	while (!(c == 2))
+	{
+		c = Menu(cur, DB);
+		cur += c;
+		printf("%i", cur);
+	}
+	return 0;
 }
